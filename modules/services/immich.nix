066f7ee593f358b2a546e9a241bd085e70d47b24
Florian Lotz <lightgaminglp@gmail.com>
2026-04-18 17:59:04 +0200

    Asdf

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -4,7 +4,7 @@
   services.immich = {
     enable = true;
     port = 2283;
-    host = "127.0.0.1";
+    host = "0.0.0.0";
     openFirewall = false;
     # Immich sets up its own Postgres and Redis automatically in NixOS
   };

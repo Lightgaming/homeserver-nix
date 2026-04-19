0579b72dbb1e4f9c4e65d6710e130360f55426c0
Florian Lotz <lightgaminglp@gmail.com>
2026-04-19 14:20:03 +0200

    Asdf

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -7,7 +7,7 @@
     backupDir = "/var/backup/vaultwarden";
     config = {
       DOMAIN = "https://vault.lotz.zip"; # Change to HTTPS if you configure certs
-      SIGNUPS_ALLOWED = false; # Turn true temporarily to create your admin account
+      SIGNUPS_ALLOWED = true; # Turn true temporarily to create your admin account
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
     };

bf92941aeb68ee05210b44d28e00e82a0a8aa6e5
Florian Lotz <florian@lotz.zip>
2026-04-20 15:01:23 +0200

    asdf

--- flake.nix
+++ flake.nix
@@ -3,14 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, vscode-server, ... }: {
     nixosConfigurations = {
       homeserver = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/homeserver/default.nix
+          vscode-server.nixosModules.default
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
         ];
       };
     };

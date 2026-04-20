faf7531da99431259fe616f84e622a53f9777d11
Florian Lotz <lightgaminglp@gmail.com>
2026-04-20 05:56:12 +0200

    Asdf

--- modules/services/adguard.nix
+++ modules/services/adguard.nix
@@ -12,6 +12,15 @@
         bootstrap_dns = [ "9.9.9.9" "1.1.1.1" ];
         upstream_dns = [ "https://dns10.quad9.net/dns-query" ];
       };
+      filtering = {
+        protection_enables = true;
+        filtering_enables = true;
+        parental_enabled = true;
+      };
+      filters = map(url: { enabled = true; url = url; }) [
+        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt"  # The Big List of Hacked Malware Web Sites
+        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt"  # malicious url blocklist
+      ];
     };
   };
 }

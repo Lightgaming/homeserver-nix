73795925d72ae9e3a88d37751d4c516d1c39cd54
Florian Lotz <lightgaminglp@gmail.com>
2026-05-03 11:54:12 +0200

    Asdf

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -3,11 +3,13 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud33;
+    nginx.enable = true;
     config.adminpassFile = "/etc/nextcloud-admin-pass";
     config.dbtype = "sqlite";
     hostName = "nextcloud.lotz.zip";
     https = true;
     settings = {
+      overwritehost = "nextcloud.lotz.zip";
       overwriteprotocol = "https";
       trusted_proxies = [ "127.0.0.1" "::1" "100.64.0.0/10" ];
     };

bf95609e493c3d9dc1f47ddff0d16889e7ca5b25
Florian Lotz <lightgaminglp@gmail.com>
2026-05-15 06:54:59 +0200

    Asdf

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -1,7 +1,8 @@
-{ ... }: 
+{ pkgs, ... }: 
 {
   services.nextcloud = {
     enable = true;
+    package = pkgs.nextcloud32;
     hostName = "nextcloud.lotz.zip";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
     config.dbtype = "sqlite";

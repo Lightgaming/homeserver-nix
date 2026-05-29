f375272562892eefe5eb01bab625d03c7f5619da
Florian Lotz <lightgaminglp@gmail.com>
2026-05-29 20:35:01 +0200

    Asdf

---
+++ modules/services/github-runner.nix
@@ -0,0 +1,10 @@
+{ pkgs, ...}:
+{
+  services.github-runners.ravr-deploy = {
+    enable = true;
+    url = "https://github.com/Lightgaming/ravr";
+    tokenFile = "/run/secrets/github-runner-token";
+    user = "hardclip";
+    extraPackages = pkgs: [ pkgs.git pkgs.openssh pkgs.docker-client pkgs.docker-compose ];
+  };
+}

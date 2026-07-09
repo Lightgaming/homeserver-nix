3de0d8d88171ddaea8ceb2440fc1dc356ef523ba
Florian Lotz <lightgaminglp@gmail.com>
2026-07-09 10:27:32 +0200

    add attic

--- flake.nix
+++ flake.nix
@@ -6,17 +6,13 @@
     
     compose2nix.url = "github:aksiksi/compose2nix";
     compose2nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    attic.url = "github:zhaofengli/attic";
-    attic.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, attic, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
       homeserver = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          attic.nixosModules.atticd
           ./hosts/homeserver/default.nix
         ];
       };
